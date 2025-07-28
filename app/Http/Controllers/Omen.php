2dc5d8fa26eed0b88b0a6d7945d808958738c223
kyoz <hadifawwazsudewo@gmail.com>
2025-07-28 10:09:18 +0700

    test

--- app/Http/Controllers/Omen.php
+++ app/Http/Controllers/Omen.php
@@ -25,7 +25,7 @@ class Omen extends Controller
         return view(
             'home',
             [
-                'title' => 'Belajar Laravel',
+                'title' => 'Belajar Laravela',
             ],
             compact('siswa')
         );
